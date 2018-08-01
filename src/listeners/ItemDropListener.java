879a79788a43f9c05d5d96ec9802382e8e2faed0
Sniper <shautacc@gmail.com>
2018-08-01 12:18:47 -0600

    Added Listener

--- src/listeners/ItemDropListener.java
+++ src/listeners/ItemDropListener.java
@@ -1,4 +1,13 @@
 package listeners;
 
-public class ItemDropListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class ItemDropListener implements Listener {
+
+    @EventHandler
+    public void dropItem(PlayerDropItemEvent e) {
+        //to do: write code
+    }
 }
